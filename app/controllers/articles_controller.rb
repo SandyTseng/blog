2d5267bbe7641abc7bda159897fbd185602d35a6
sandy <sandytseng0828@gmail.com>
2016-01-27 12:49:09 +0800

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+	def new
+	end
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+	def create
+  @article = Article.new(article_params)
+ 
+  @article.save
+  redirect_to @article
+  end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
