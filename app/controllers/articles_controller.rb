f2df7537b3cdbb1652c826ef061a4ba056242257
sandy <sandytseng0828@gmail.com>
2016-01-30 22:05:13 +0800

    crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "sandy", password: "secret", except: [:index, :show]
 
   def index
     @articles = Article.all.reverse
@@ -11,18 +12,39 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def edit
+  @article = Article.find(params[:id])
+  end
+
 
 	def create
   @article = Article.new(article_params)
  
-  @article.save
+  if @article.save
   redirect_to @article
+  else
+    render 'new'
   end
-  http_basic_authenticate_with name: "sandy", password: "secret", except: [:index, :show]
-
+  end
+  
+  def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+  end 
+  def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
 private
   def article_params
     params.require(:article).permit(:title, :text)
   end
-
 end
